433a84e5084c6e5ef9c11765b28233a40403f32f
fdurovsky <durovsky987@gmail.com>
2015-05-15 13:33:36 +0200

    initial commit

---
+++ include/ros_profinet_experimental/ros_profinet_experimental_lib.h
@@ -0,0 +1,37 @@
+/*********************************************************************************************//**
+* @file ros_profinet_experimental_lib.h
+* 
+* ros_profinet_experimental header 
+* 
+* Copyright {2015} {Frantisek Durovsky}
+* 
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+
+*      http://www.apache.org/licenses/LICENSE-2.0
+
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+   
+* *********************************************************************************************/
+#ifndef ROS_PROFINET_EXPERIMENTAL_LIB_H
+#define ROS_PROFINET_EXPERIMENTAL_LIB_H
+
+#include <ros/ros.h>
+
+class profinet
+{
+public:
+    profinet();
+    ~profinet();
+
+private:
+
+};
+
+
+#endif //ROS_PROFINET_EXPERIMENTAL_LIB_H
---
+++ src/ros_profinet_experimental_lib.cpp
@@ -0,0 +1,36 @@
+/*********************************************************************************************//**
+* @file ros_profinet_experimental_lib.cpp
+* 
+* ros_profinet_experimental library 
+* 
+* Copyright {2015} {Frantisek Durovsky}
+* 
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+
+*      http://www.apache.org/licenses/LICENSE-2.0
+
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+   
+* *********************************************************************************************/
+#ifndef ROS_PROFINET_EXPERIMENTAL_LIB_CPP
+#define ROS_PROFINET_EXPERIMENTAL_LIB_CPP
+
+#include <ros_profinet_experimental/ros_profinet_experimental_lib.h>
+
+profinet::profinet()
+{
+
+}
+
+profinet::~profinet()
+{
+
+}
+
+#endif //ROS_PROFINET_EXPERIMENTAL_LIB_CPP
